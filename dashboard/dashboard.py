d116d47fd6444ab68cfa2f65a2ea0ea3ec71f23d
pael611 <122434166+pael611@users.noreply.github.com>
2025-03-08 19:20:59 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,8 +7,8 @@ sns.set(style='dark')
 st.header('Dashboard')
 
 # Import dataset yang akan kita gunakan (csv)
-df1 = pd.read_csv('dashboard/Dongsi.csv')
-df2 = pd.read_csv('dashboard/Shunyi.csv')
+df1 = pd.read_csv('dongsi.csv')
+df2 = pd.read_csv('shunyi.csv')
 
 # Menampilkan dataset
 st.subheader('Dataset Dongsi')
